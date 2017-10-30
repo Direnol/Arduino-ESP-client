fa05bf2268f63766dd004731d19257e165086213
Roman Mingazeev <direnol@yandex.ru>
2017-10-30 18:16:47 +0700

    init

--- src/client.cpp
+++ src/client.cpp
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 #include <Arduino.h>
+
 HardwareSerial &ESPport = Serial;
 ////////////////////// RX, TX
 
@@ -8,109 +9,103 @@ HardwareSerial &ESPport = Serial;
 // HardwareSerial & ESPport = Serial1;
 
 void otvet_klienty(int ch_id);
+
 String GetResponse(const String &AT_Command, unsigned long wait);
+
 void clearSerialBuffer();
+
 boolean connectWiFi(const String &NetworkSSID, const String &NetworkPASS);
+
 void clearBuffer();
 
-const int ledPin =  13;
+const int ledPin = 13;
 uint8_t ledState = HIGH;
 #define BUFFER_SIZE 128
 char buffer[BUFFER_SIZE];
-String vklotkl = "";
+String button;
 
 void setup()
 {
     pinMode(ledPin, OUTPUT);
     //Serial.begin(9600); // Терминал
     ESPport.begin(115200); // ESP8266
-    clearSerialBuffer(); // очистка буфера Serial
-    Serial.println("RESET 3,5 sek");
-    Serial.println(GetResponse("AT+RESTORE",3400)); // перезагрузка ESP
-    Serial.println(GetResponse("AT+CWMODE=1",300)); // режим клиента
-    connectWiFi("myrouter","parolparol"); // подключаемся к домашнему роутеру (имя точки, пароль)
-    Serial.println(GetResponse("AT+CIPMODE=0",300)); // сквозной режим передачи данных.
-    Serial.println(GetResponse("AT+CIPMUX=1",300)); // multiple connection.
-    Serial.print("Start TCP-server: ");
-    Serial.println(GetResponse("AT+CIPSERVER=1,88", 300)); // запускаем ТСР-сервер на 88-ом порту
-    Serial.println(GetResponse("AT+CIPSTO=2", 300)); // таймаут сервера 2 сек
-    Serial.println(GetResponse("AT+CIFSR", 300)); // узнаём адрес
-    digitalWrite(ledPin,ledState);
+    clearSerialBuffer();
+
+    GetResponse("AT+RST", 3400); // перезагрузка ESP
+    GetResponse("AT+CWMODE=1", 300); // режим клиента
+    connectWiFi("myrouter", "parolparol"); // подключаемся к домашнему роутеру (имя точки, пароль)
+    GetResponse("AT+CIPMODE=0", 300); // сквозной режим передачи данных.
+    GetResponse("AT+CIPMUX=1", 300); // multiple connection.
+
+    GetResponse("AT+CIPSERVER=1,88", 300); // запускаем ТСР-сервер на 88-ом порту
+    GetResponse("AT+CIPSTO=2", 300); // таймаут сервера 2 сек
+    GetResponse("AT+CIFSR", 300); // узнаём адрес
+    digitalWrite(ledPin, ledState);
 }
+
 ///////////////////основной цикл, принимает запрос от клиента///////////////////
 void loop()
 {
-    int ch_id, packet_len; // объявляем переменные
+    int ch_id, packet_len;
     char *pb;
-    ESPport.readBytesUntil('\n', buffer, BUFFER_SIZE); // читаем строку из есп в переменную "buffer"
+    ESPport.readBytesUntil('\n', buffer, BUFFER_SIZE);
 
-    if(strncmp(buffer, "+IPD,", 5)==0) // если в "buffer" есть строка из пяти символов - "+IPD," тогда -
-    {                                 // (СИшная функция "strncmp" сравнивает содержимое "buffer" с заданой строкой "+IPD,")
-        sscanf(buffer+5, "%d,%d", &ch_id, &packet_len); // - вычитываем из "buffer" и засовываем в переменные (ch_id и packet_len) id клиента и длину пакета
-        if (packet_len > 0)                             // (СИшная функция "sscanf")
-        {
-            pb = buffer+5;
-            while(*pb!=':') pb++;
+    if (strncmp(buffer, "+IPD,", 5) == 0) {
+        sscanf(buffer + 5, "%d,%d", &ch_id, &packet_len);
+        if (packet_len > 0) {
+            pb = buffer + 5;
+            while (*pb != ':') pb++;
             pb++;
-            if(strncmp(pb, "GET / ", 6) == 0) // если в "pb" есть фраза "GET / ", то
-            {
-                Serial.println(buffer); // выводим в консоль содержимое buffer (просто чтоб поглядеть чего прилетело, для работы эти пять строк можно закомментировать)
-                Serial.print("get led from ch :");
-                Serial.println(ch_id);
-                delay(100);
+            if ((strncmp(pb, "GET / ", 6) == 0) || (strncmp(pb, "GET /?", 6) == 0)) {
                 clearSerialBuffer();
 
-                if(ledState == LOW) // узнаём состояние светика
-                {
+                if (ledState == LOW) {
                     ledState = HIGH;
-                    vklotkl = "VKL";
-                }
-
-                else
-                {
+                    button = "VKL";
+                } else {
                     ledState = LOW;
-                    vklotkl = "OTKL";
+                    button = "OTKL";
                 }
 
-                digitalWrite(ledPin, ledState); // инвертируем состояние светика
-                otvet_klienty(ch_id);  // отправляемся в функцию формирования ответа клиенту (и передаём ей id клиента)
+                digitalWrite(ledPin, ledState);
+                otvet_klienty(ch_id);
             }
         }
     }
     clearBuffer();
 }
+
 //////////////////////формирование ответа клиенту////////////////////
 void otvet_klienty(int ch_id)
 {
-    // собираем строку для клиента (заголовок пакета)
     String Header;
 
-    Header =  "HTTP/1.1 200 OK\r\n";
+    Header = "HTTP/1.1 200 OK\r\n";
     Header += "Content-Type: text/html\r\n";
     Header += "Connection: close\r\n";
 
-    // содержимое пакета
     String Content;
 
-    Content = "LED 13: " + vklotkl;
+    Content = "<body><form action='' method='GET'><input type='submit' value='VKL/OTKL'> " + button;
+    Content += "</form></body></html>";
 
     Header += "Content-Length: ";
-    Header += (int)(Content.length());
+    Header += (int) (Content.length());
     Header += "\r\n\r\n";
 
-    ESPport.print("AT+CIPSEND="); // ответ клиенту: ат-команда, id-клиента, , , длина пакета
+    ESPport.print("AT+CIPSEND="); // ответ клиенту
     ESPport.print(ch_id);
     ESPport.print(",");
-    ESPport.println(Header.length()+Content.length());
+    ESPport.println(Header.length() + Content.length());
     delay(20);
 
-    if(ESPport.find(const_cast<char *>(">"))) // ожидание от esp приглашения ввода
-    {
-        ESPport.print(Header); // отправка заголовка
-        ESPport.print(Content); // отправка содержимого пакета
-        delay(110);
+    if (ESPport.find(const_cast<char *>(">"))) {
+        ESPport.print(Header);
+        ESPport.print(Content);
+        delay(200);
     }
 }
+
 /////////////////////отправка АТ-команд/////////////////////
 String GetResponse(const String &AT_Command, unsigned long wait)
 {
@@ -118,12 +113,11 @@ String GetResponse(const String &AT_Command, unsigned long wait)
 
     ESPport.println(AT_Command);
     delay(wait);
-    while (ESPport.available() >0 )
-    {
+    while (ESPport.available() > 0) {
         char c = static_cast<char>(ESPport.read());
         tmpData += c;
 
-        if ( tmpData.indexOf(AT_Command) > -1 )
+        if (tmpData.indexOf(AT_Command) > -1)
             tmpData = "";
         else
             tmpData.trim();
@@ -131,30 +125,33 @@ String GetResponse(const String &AT_Command, unsigned long wait)
     }
     return tmpData;
 }
+
 //////////////////////очистка ESPport////////////////////
 void clearSerialBuffer()
 {
-    while ( ESPport.available() > 0 )
-    {
+    while (ESPport.available() > 0) {
         ESPport.read();
     }
 }
+
 ////////////////////очистка буфера////////////////////////
 void clearBuffer()
 {
-    for (int i =0;i<BUFFER_SIZE;i++ )
-    {
-        buffer[i]=0;
+    for (char &i : buffer) {
+        i = 0;
     }
 }
+
 ////////////////////подключение к wifi/////////////////////
-boolean connectWiFi(const String &NetworkSSID,const String &NetworkPASS)
+boolean connectWiFi(const String &NetworkSSID, const String &NetworkPASS)
 {
     String cmd = "AT+CWJAP=\"";
     cmd += NetworkSSID;
     cmd += "\",\"";
     cmd += NetworkPASS;
     cmd += "\"";
-    Serial.println(cmd);
-    Serial.println(GetResponse(cmd,6500));
+
+    ESPport.println(cmd);
+    delay(6500);
+
 }
